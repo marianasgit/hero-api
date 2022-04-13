b4f99f630f0c2d5f5f454b365ac1154a479433bc
MARIANA GOMES DOS SANTOS <you@example.com>
2022-04-13 16:43:28 -0300

    comecando slide

--- main.js
+++ main.js
@@ -12,56 +12,59 @@ const criarPersonagem = ({ image, biography}) => {
   const card = document.createElement("div");
 
   card.innerHTML = `
-      <h3>Biography</h3>
-      <img src="${image.url}" class="front"></img>
-      <ul class="back">
-        <li>
-          <h4>Full Name:</h3>
-          <span>${biography["full-name"]}</span>
-        </li>
-        <li>
-          <h4>Alter-egos:</h3>
-          ${biography["alter-egos"]}
-        </li>
-        <li>
-          <h4>Aliases:</h3>
-          ${biography.aliases}
-        </li>
-        <li>
-          <h4>Place of Birth:</h3>
-          ${biography["place-of-birth"]}
-        </li>
-        <li>
-          <h4>First Appearance:</h3>
-          ${biography["first-appearance"]}
-        </li>
-        <li>
-          <h4>Publisher:</h3>
-          ${biography.publisher}
-        </li>
-        <li>
-          <h4>Alignment:</h3>
-          ${biography.alignment}
-        </li>
-      </ul>
-    </div>
+    <img src="${image.url}" class="front"></img>
+    <h3>Biography</h3>
+    <ul>
+      <li>
+        <h4>Full Name:</h3>
+        <span>${biography["full-name"]}</span>
+      </li>
+      <li>
+        <h4>Alter-egos:</h3>
+        ${biography["alter-egos"]}
+      </li>
+      <li>
+        <h4>Aliases:</h3>
+        ${biography.aliases}
+      </li>
+      <li>
+        <h4>Place of Birth:</h3>
+        ${biography["place-of-birth"]}
+      </li>
+      <li>
+        <h4>First Appearance:</h3>
+        ${biography["first-appearance"]}
+      </li>
+      <li>
+        <h4>Publisher:</h3>
+        ${biography.publisher}
+      </li>
+      <li>
+        <h4>Alignment:</h3>
+        ${biography.alignment}
+      </li>
+    </ul>
+  </div>
 
     
   `;
 
   return card;
-
-  // const img = document.createElement("img");
-  // img.src = image.url;
-
-  // const bio = document.createElement("bio");
-  // bio.src = biography;
-
-  // const power = document.createElement("power");
-  // power.src = powerstats;
-
-  // console.log (biography);
 };
+
+var swiper = new Swiper(".mySwiper", {
+         slidesPerView: 1,
+         spaceBetween: 30,
+         loop: true,
+         pagination: {
+           el: ".swiper-pagination",
+           clickable: true,
+         },
+         navigation: {
+           nextEl: ".swiper-button-next",
+           prevEl: ".swiper-button-prev",
+         },
+       });
 
 const carregarPersonagens = async () => {
   const container = document.getElementById("imagem-container");
